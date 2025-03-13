82c9bc208b2d03bc404bf630e5f781aa7fb766b8
anggatrinugraha <anggatrinugraha@localhost>
2025-03-13 13:25:25 +0700

    commit 1

--- Test Suites/Buka Web.ts
+++ Test Suites/Buka Web.ts
@@ -11,4 +11,25 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>4388d645-3592-4a65-bcde-73ef1bd1c11e</testSuiteGuid>
+   <testCaseLink>
+      <guid>e2fb6c61-82eb-4274-8da4-3a1a4da25b96</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/POSITIVE - Buka Web</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c6d5c20e-3ed8-4793-a78a-8f073d6fe6c5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/POSITIVE - Login Berhasil</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e827ec8a-607b-45f9-ad41-bf9caa5568cc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/NEGATIVE - Password salah</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
