6df587bb2466bae9c0c7ad9b118b52b2dcb4f00f
anggatrinugraha <anggatrinugraha@localhost>
2025-03-13 13:12:02 +0700

    commit 1

---
+++ Test Suites/Buka Web.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Buka Web</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>4388d645-3592-4a65-bcde-73ef1bd1c11e</testSuiteGuid>
+</TestSuiteEntity>
