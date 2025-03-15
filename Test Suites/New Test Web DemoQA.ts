403d7dc5ce040e4b70899cd94680ac16f6ce9675
anggatrinugraha <anggatrinugraha@localhost>
2025-03-15 07:09:40 +0700

    commit new

---
+++ Test Suites/New Test Web DemoQA.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Web DemoQA</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>f9b47397-c99b-4f6f-87ff-6d21fba03945</testSuiteGuid>
+</TestSuiteEntity>
